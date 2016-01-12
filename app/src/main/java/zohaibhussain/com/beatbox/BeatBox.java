25e16430e9692221cba8c7e7aeb2251ebad0a0da
Zohaib-AHussain <zowaib@gmail.com>
2016-01-11 20:43:43 -0500

    Adding in a class that is being used a manager for fetching and loading sounds

---
+++ app/src/main/java/zohaibhussain/com/beatbox/BeatBox.java
@@ -0,0 +1,46 @@
+package zohaibhussain.com.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zohaibhussain on 2016-01-11.
+ */
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private List<Sound> mSounds  = new ArrayList<>();
+
+    private AssetManager mAssets;
+
+    public BeatBox(Context context){
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try{
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException e) {
+            Log.e(TAG, "Could not list assets", e);
+            return;
+        }
+
+        for (String filename: soundNames){
+            String assetPath = SOUNDS_FOLDER + "/" +filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds(){
+        return mSounds;
+    }
+}
